1b12437e85bcba86f8f8494e72791a54927a342f
Ardiono Roma Nugraha <ardionoroma@gmail.com>
2020-08-10 12:37:02 +0700

    initial commit

---
+++ src/main/java/com/benica/catatcovid/CatatcovidApplication.java
@@ -0,0 +1,13 @@
+package com.benica.catatcovid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatatcovidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatatcovidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/benica/catatcovid/CatatcovidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benica.catatcovid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatatcovidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
